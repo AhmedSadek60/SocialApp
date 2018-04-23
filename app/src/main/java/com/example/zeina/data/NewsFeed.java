35a08b5a9351cb660c109528ea5dba0c9ec247b3
Ahmed Abdelrhaman <ahmed_sadek60@hotmail.com>
2018-04-24 00:08:40 +0200

    initial commit

---
+++ app/src/main/java/com/example/zeina/data/NewsFeed.java
@@ -0,0 +1,14 @@
+package com.example.zeina.data;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewsFeed extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_feed);
+
+
+    }
+}
